2f0a28c565f42afc0310909249481a0fe1f3dfdb
Khalid G <kgibson4795@gmail.com>
2023-08-28 09:59:26 -0400

    updated my prompts with when function

--- index.js
+++ index.js
@@ -23,51 +23,78 @@ inquirer
          'Add Role',
          'View All Departments',
          'Add Department',
-         'Quit']
+         'Quit'],  
       },
       {
         type: 'input',
         name: 'addEmployee',
         message: "What is the name of the employee?",
+        when: function() {
+          return answers.start === 'Add Employee'
+        }
       },
       {
         type: 'input',
         name: 'updateRole',
-        message: "What is the name of the employee?",
+        message: "What is the new role of the employee?",
+        when: function() {
+          return answers.addEmployee === 'Update Employee Role'
+        }
       },
       {
         type: 'input',
         name: 'updateRole',
         message: "How would you like to update the role of the employee?",
+        when: function() {
+          return answers.start === 'Add Employee'
+        }
       },
       {
         type: 'input',
         name: 'viewRoles',
         message: "What is the name of the employee?",
+        when: function() {
+          return answers.start === 'Add Employee'
+        }
       },
       {
         type: 'input',
         name: 'addRole',
         message: "What is the name of the role?",
+        when: function() {
+          return answers.start === 'Add Role'
+        }
       },
       {
         type: 'input',
         name: 'roleSalary',
         message: "What is the salary of the role?",
+        when: function() {
+          return answers.start === 'Add Employee'
+        }
       },
       {
         type: 'input',
         name: 'roleDepartment',
         message: "Which department does this role belong to?",
+        when: function() {
+          return answers.start === 'Add Employee'
+        }
       },
       {
         type: 'input',
         name: 'viewDepartments',
         message: "What is the name of the employee?",
+        when: function() {
+          return answers.start === 'Add Employee'
+        }
       },
       {
         type: 'input',
         name: 'addDepartment',
         message: "What is the name of the department?",
+        when: function() {
+          return answers.start === 'Add Department'
+        }
       },
      ])
