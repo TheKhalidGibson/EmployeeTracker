5203245821679ade122326894cbe85ae52b753db
Khalid G <kgibson4795@gmail.com>
2023-08-28 09:59:35 -0400

    minor changes

--- schema.sql
+++ schema.sql
@@ -33,26 +33,34 @@ VALUES ("Engineering"),
        ("Sales"),
        ("Legal");
 
-INSERT INTO role (name, department)
-VALUES ("Software Engineer", 1),
-       ("Design Engineer", 1),
-       ("Systems Engineer", 1),
-       ("Financial Analyst", 2),
-       ("Financial Management", 2),
-       ("Budget Analyst", 2),
-       ("Director of Sales", 3),
-       ("Sales Representative", 3),
-       ("Account Executive", 3),
-       ("Chief Legal Officer", 4),
-       ("Legal Consultant", 4),
-       ("Legal Secretary", 4);
+INSERT INTO role (title, salary, department_id)
+VALUES ("Software Engineer", 100000, 1),
+       ("Design Engineer", 90000, 1),
+       ("Systems Engineer", 110000, 1),
+       ("Financial Analyst", 80000, 2),
+       ("Financial Management", 130000, 2),
+       ("Budget Analyst", 80000, 2),
+       ("Director of Sales", 200000, 3),
+       ("Sales Representative", 60000, 3),
+       ("Account Executive", 100000, 3),
+       ("Chief Legal Officer", 350000, 4),
+       ("Legal Consultant", 120000, 4),
+       ("Legal Secretary", 50000, 4);
 
-INSERT INTO employee (name, department)
-VALUES ("Robert Smith", 1),
-       ("Steve Knight", 2),
-       ("Barbara Kent", 3),
-       ("Latasha Miller", 4),
-       ("Mackenzie Waters", 1),
-       ("Malcolm Harkens", 2),
-       ("Rachel Letterman", 3),
-       ("Buddy Troy", 4);
+INSERT INTO employee (first_name, last_name, role_id, manager_id)
+VALUES ("Robert", "Smith", 1,),
+       ("Steve", "Knight", 2),
+       ("Barbara", "Kent", 3),
+       ("Latasha", "Miller", 4),
+       ("Mackenzie", "Waters", 5),
+       ("Malcolm", "Harkens", 6),
+       ("Rachel", "Letterman", 7),
+       ("Buddy", "Troy", 8),
+       ("Roxanne", "Sampson", 9),
+       ("Sal", "Kirk", 10),
+       ("Betty", "Cash", 11),
+       ("Lacey", "Mitchell", 12),
+       ("Mary", "Willis", 1),
+       ("Jamal", "Harris", 2),
+       ("Riley", "Lakes", 3),
+       ("Brian", "Towers", 4);
