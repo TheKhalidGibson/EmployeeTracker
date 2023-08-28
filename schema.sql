7fbf1e587c9c7034b92b35c69b2f5cb08db03ea5
Khalid G <kgibson4795@gmail.com>
2023-08-27 23:49:50 -0400

    added my first draft of seeds to the schema

--- schema.sql
+++ schema.sql
@@ -26,3 +26,33 @@ CREATE TABLE employee (
   FOREIGN KEY (role_id)
   REFERENCES role(id)
 );
+
+INSERT INTO department (name)
+VALUES ("Engineering"),
+       ("Finance"),
+       ("Sales"),
+       ("Legal");
+
+INSERT INTO role (name, department)
+VALUES ("Software Engineer", 1),
+       ("Design Engineer", 1),
+       ("Systems Engineer", 1),
+       ("Financial Analyst", 2),
+       ("Financial Management", 2),
+       ("Budget Analyst", 2),
+       ("Director of Sales", 3),
+       ("Sales Representative", 3),
+       ("Account Executive", 3),
+       ("Chief Legal Officer", 4),
+       ("Legal Consultant", 4),
+       ("Legal Secretary", 4);
+
+INSERT INTO employee (name, department)
+VALUES ("Robert Smith", 1),
+       ("Steve Knight", 2),
+       ("Barbara Kent", 3),
+       ("Latasha Miller", 4),
+       ("Mackenzie Waters", 1),
+       ("Malcolm Harkens", 2),
+       ("Rachel Letterman", 3),
+       ("Buddy Troy", 4);
