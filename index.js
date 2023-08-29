e9f18d21251e15fed728090feba6f8b65c61e529
Khalid G <kgibson4795@gmail.com>
2023-08-28 21:04:14 -0400

    made functions and adjusted my prompts

--- index.js
+++ index.js
@@ -5,47 +5,66 @@ const cTable = require('console.table');
 
 // let selectedChoice =
 
-const answers = 'input'
+const mysql = require('mysql2')
 
+require('dotenv').config()
 
+const connection = mysql.createConnection({
+  host: 'localhost',
+  user: process.env.DB_USER,
+  database: process.env.DB_NAME,
+  password: process.env.DB_PW
+});
 
-inquirer
+mainMenu()
+
+function mainMenu() {
+
+  inquirer
     .prompt([
       {
         type: 'list',
         name: 'start',
         message: "What would you like to do? (Use arrow keys)",
-        choices: 
-        ['View All Employees',
-         'Add Employee',
-         'Update Employee Role',
-         'View All Roles',
-         'Add Role',
-         'View All Departments',
-         'Add Department',
-         'Quit'],  
+        choices:
+          ['View All Employees',
+            'Add Employee',
+            'Update Employee Role',
+            'View All Roles',
+            'Add Role',
+            'View All Departments',
+            'Add Department',
+            'Quit'],
       },
       {
         type: 'input',
-        name: 'addEmployee',
+        name: 'first_name',
         message: "What is the name of the employee?",
-        when: function() {
+        when: function (answers) {
           return answers.start === 'Add Employee'
-        } 
+        }
+      },
+      {
+        type: 'input',
+        name: 'last_name',
+        message: "What is the name of the employee?",
+        when: function (answers) {
+          return answers.start === 'Add Employee'
+        }
       },
       {
         type: 'input',
         name: 'updateRole',
         message: "What is the new role of the employee?",
-        when: function() {
-          return answers.addEmployee === 'Employee'
+        when: function (answers) {
+          return answers.addEmployee === 'Update Employee Role'
         }
       },
       {
         type: 'input',
         name: 'updateRole',
         message: "How would you like to update the role of the employee?",
-        when: function() {
+        when: function (answers) {
           return answers.start === 'Add Employee'
         }
       },
@@ -53,7 +72,7 @@ inquirer
         type: 'input',
         name: 'viewRoles',
         message: "What is the name of the employee?",
-        when: function() {
+        when: function (answers) {
           return answers.start === 'Add Employee'
         }
       },
@@ -61,7 +80,7 @@ inquirer
         type: 'input',
         name: 'addRole',
         message: "What is the name of the role?",
-        when: function() {
+        when: function (answers) {
           return answers.start === 'Add Role'
         }
       },
@@ -69,7 +88,7 @@ inquirer
         type: 'input',
         name: 'roleSalary',
         message: "What is the salary of the role?",
-        when: function() {
+        when: function (answers) {
           return answers.start === 'Add Employee'
         }
       },
@@ -77,7 +96,7 @@ inquirer
         type: 'input',
         name: 'roleDepartment',
         message: "Which department does this role belong to?",
-        when: function() {
+        when: function (answers) {
           return answers.start === 'Add Employee'
         }
       },
@@ -85,16 +104,81 @@ inquirer
         type: 'input',
         name: 'viewDepartments',
         message: "What is the name of the employee?",
-        when: function() {
+        when: function (answers) {
           return answers.start === 'Add Employee'
         }
       },
       {
         type: 'input',
-        name: 'addDepartment',
+        name: 'name',
         message: "What is the name of the department?",
-        when: function() {
+        when: function (answers) {
           return answers.start === 'Add Department'
         }
       },
-     ])
+    ]) 
+    .then((answers) =>{
+
+      if (answers.start === 'View All Departments') {
+        viewDepartments()
+      } else if (answers.start === 'View All Roles') {
+        viewRoles()
+      } else if (answers.start === 'View All Employees') {
+        viewEmployees()
+      } else if (answers.start === 'Add Department') {
+        newDepartment({name:answers.name})
+      }
+    })
+
+}
+
+function viewDepartments() {
+  
+  connection.query(
+    'SELECT * FROM department',
+    function(err, results,) {
+      if(err){console.log(err)}
+      console.table(results); // results contains rows returned by server
+      mainMenu()
+    }
+  );
+
+}
+function viewRoles() {
+  
+  connection.query(
+    'SELECT * FROM role',
+    function(err, results,) {
+      if(err){console.log(err)}
+      console.table(results); // results contains rows returned by server
+      mainMenu()
+    }
+  );
+
+}
+function viewEmployees() {
+  
+  connection.query(
+    'SELECT * FROM employee',
+    function(err, results,) {
+      if(err){console.log(err)}
+      console.table(results); // results contains rows returned by server
+      mainMenu()
+    }
+  );
+
+}
+
+function newDepartment(object) {
+  console.log(object)
+  connection.query(
+    'INSERT INTO department SET ?',
+    object,
+    function(err, results,) {
+      if(err){console.log(err)}
+      console.table(results); // results contains rows returned by server
+      mainMenu()
+    }
+  )
+  
+}
